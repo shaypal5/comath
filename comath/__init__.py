1333e3b96242a5bad9d3e699ffd58a1597fdc89f
Shay Palachy <shaypal5@gmail.com>
2017-12-13 14:58:17 +0200

    fixing package discovery

--- comath/__init__.py
+++ comath/__init__.py
@@ -1,15 +1,13 @@
 """Utility pure-Python 3 decorators."""
 
-from . import (
-    array,
-    func,
-    metric,
-    segment,
-)
-# try:
-#     del comath
-# except NameError: # pragma: no cover
-#     pass
+import comath.array
+import comath.func
+import comath.metric
+import comath.segment
+try:
+    del comath
+except NameError: # pragma: no cover
+    pass
 
 from ._version import get_versions
 __version__ = get_versions()['version']
